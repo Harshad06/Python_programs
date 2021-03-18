c542a55a677655f5b1fb0616a4a2ecf5daf9f7d9
Harshad06 <harshad.shringi@gmail.com>
2021-03-18 08:39:57 +0530

    commit code

--- py-programs/01_test.py
+++ py-programs/01_test.py
@@ -1,17 +1,1 @@
 
-
-s = "wowow"
-rev = "".join(reversed(s))
-print(s)
-print(rev)
-
-
-'''
-Only printing "reversed(s)" will give --- <reversed object at 0x01C1E700>
-but rev = "".join(reversed(s))  will give string.
-'''
-
-if s == rev:
-    print(f'Yes')
-else:
-    print(f'No')
