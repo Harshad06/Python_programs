0dd4a640f2471c0417e7dc3f82f1fc559d8efb0f
Harshad06 <harshad.shringi@gmail.com>
2021-03-29 09:50:24 +0530

    push code

---
+++ py-programs/mutilple_input.py
@@ -0,0 +1,12 @@
+'''
+Name = input("Enter your name : ")
+Age = input("Enter your age : ")
+
+print(Name, Age)
+'''
+
+# Now, concept of multiple inputs:
+# Here, split happens on " " ---> a space between name and age
+
+name, age = input("Enter your name and age : ").split()
+print(name, age)
