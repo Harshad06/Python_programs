159f03ef5eed0036fdf6b9b91badd82d76a5929f
Harshad06 <harshad.shringi@gmail.com>
2021-04-02 16:57:56 +0530

    note added

--- decorator/generator.py
+++ decorator/generator.py
@@ -18,7 +18,13 @@ print(even_list)
 
 
 '''
-Generator functions return a generator object. 
-Generator objects are used either by calling the next method on the generator object or 
-    using the generator object in a “for in” loop 
+> A generator-function is defined like a normal function, but whenever it needs to generate a value, 
+    it does so with the yield keyword rather than return. 
+
+> If the body of a def contains yield, the function automatically becomes a generator function.
+
+> Generator functions return a generator object. 
+
+> Generator objects are used either by calling the next method on the generator object or 
+    using the generator object in a “for in” loop. 
 '''
