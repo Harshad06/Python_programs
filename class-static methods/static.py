43557b7008b350cd63143c04296f344a1b23894e
Harshad06 <harshad.shringi@gmail.com>
2021-04-02 16:06:43 +0530

    comment

--- class-static methods/static.py
+++ class-static methods/static.py
@@ -11,7 +11,5 @@ print(student.find_min(20))
 
 
 # @static method decorator
-# Static method is used to call function/property without 
-# instantiating the class first.
-# 
-#  Static methods can also be accessed via class instances/or objects.
+# Static method is used to call function/property without instantiating the class first.
+# Static methods can also be accessed via class instances/or objects.
