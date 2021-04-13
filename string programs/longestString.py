082c404f98667d10d9b3613351ee4ff10825f82b
Harshad06 <harshad.shringi@gmail.com>
2021-04-13 20:02:14 +0530

    code commit

--- string programs/longestString.py
+++ string programs/longestString.py
@@ -9,13 +9,12 @@ test_list = ['gfg', 'is', 'best', 'for', 'geeks']
 # printing original list
 print("The original list : " + str(test_list))
 
-# Longest String in list
-# using loop
+# Longest String in list using loop
 max_len = -1
-for ele in test_list:
-	if len(ele) > max_len:
-		max_len = len(ele)
-		res = ele
+for e in test_list:
+	if len(e) > max_len:
+		max_len = len(e)
+		res = e
 
 # printing result
 print("Maximum length string is : " + res)
