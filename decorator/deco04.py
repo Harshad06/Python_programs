2b5645070b20a74a88ed23dab3b4b81a978b6f8c
Harshad06 <harshad.shringi@gmail.com>
2021-04-16 17:00:26 +0530

    code commit

---
+++ decorator/deco04.py
@@ -0,0 +1,16 @@
+
+def inc(x):
+    return x + 1
+
+
+def dec(x):
+    return x - 1
+
+
+def operate(func, x):
+    result = func(x)
+    return result
+
+
+print(operate(inc,5))
+print(operate(dec,5))
