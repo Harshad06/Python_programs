abf1e997a285262d579edf4c1851f4ecde524e35
Harshad06 <harshad.shringi@gmail.com>
2021-04-16 21:52:38 +0530

    add

---
+++ string programs/01_test.py
@@ -0,0 +1,21 @@
+'''
+s = "Hello"
+print(type(s))
+
+print(s.__getitem__(3))
+
+print(s[3])
+
+print(~4)
+print(~~4)
+
+a = 5
+b = 3
+print(a//b)
+
+# in = 5 
+
+#print("Hello"+1+2+3)    # Error
+
+print("1" == 1)  # False
+'''
