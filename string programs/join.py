9b00749cd3e0aea3f6333ab1ad593c27e53ce6fe
Harshad06 <harshad.shringi@gmail.com>
2021-04-16 18:58:22 +0530

    code commit

---
+++ string programs/join.py
@@ -0,0 +1,14 @@
+
+s = "I love Python Programming Language"
+
+s1 = s.split()
+print(s1)
+
+s2 = " ".join(s1)
+print(s2)
+
+s2 = "-".join(s1)
+print(s2)
+
+s2 = "**".join(s1)
+print(s2)
---
+++ string programs/split.py
@@ -0,0 +1,11 @@
+
+s = "I love Python Programming Language"
+
+s1 = s.split()
+print(s1)
+# same as above
+s1 = s.split(" ")
+print(s1)
+
+s1 = s.split("P")
+print(s1)
