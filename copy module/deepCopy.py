31380ac5d3cec963fdbe0e56bce9a06e5a740699
Harshad06 <harshad.shringi@gmail.com>
2021-04-16 22:05:46 +0530

    code commit

---
+++ copy module/deepCopy.py
@@ -0,0 +1,12 @@
+
+import copy
+
+old_list = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+new_list = copy.deepcopy(old_list)
+
+old_list.append([4,4,4])
+
+old_list[1][0] = 'BB'
+
+print("Old list:", old_list)
+print("New list:", new_list)
