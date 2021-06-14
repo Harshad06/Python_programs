d4e16873f13b505cbeb4206e932abc0f0b003f2c
Harshad06 <harshad.shringi@gmail.com>
2021-06-14 19:20:54 +0530

    commit

---
+++ Higher complexity programs/removeDuplicates.py
@@ -0,0 +1,22 @@
+
+# Python code to remove duplicate elements
+
+"""
+Input : [2, 4, 10, 20, 5, 2, 20, 4]
+Output : [2, 4, 10, 20, 5]
+
+Input : [28, 42, 28, 16, 90, 42, 42, 28]
+Output : [28, 42, 16, 90]
+"""
+
+
+def Remove(duplicate):
+    final_lst = []
+    for num in duplicate:
+        if num not in final_lst:
+            final_lst.append(num)
+    return final_lst
+
+# Driver code
+dup = [2, 4, 10, 20, 5, 2, 20, 4]
+print(Remove(dup))               # [2, 4, 10, 20, 5]
