c11cca3c96888e003e93c1da5c37fa601abf46a1
Harshad06 <harshad.shringi@gmail.com>
2021-06-25 18:27:15 +0530

    commit

---
+++ PANDAS/joinsIdenticalData.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+
+df1 = pd.DataFrame([1,1], columns=['col_A'] )
+#print("DataFrame #1: \n", df1)
+
+df2 = pd.DataFrame([1,1,1], columns=['col_A'] )
+#print("DataFrame #2: \n", df2)
+
+df3 = pd.merge(df1, df2, on='col_A', how='inner')
+print("DataFrame after inner join: \n", df3)
+
+
+
+# Output:    2x3 --> 6 times it will be printed. [one-many operation] 
+# It maybe any type of join ---> inner/outer/right/left  
+'''
+DataFrame after inner join: 
+    col_A
+0      1 
+1      1 
+2      1 
+3      1 
+4      1 
+5      1
+'''
