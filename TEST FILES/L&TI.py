f1a81d4a0695900fcd54ed0f186e65590fe82b2e
Harshad06 <harshad.shringi@gmail.com>
2021-07-08 11:51:25 +0530

    commit

---
+++ TEST FILES/L&TI.py
@@ -0,0 +1,42 @@
+'''
+peak find length??
+lst = [3,2,4,6,5,9,12,10]
+
+
+
+sum 10 for any 2 numbers??
+l1 = [3,2,4,6,5,9,12,10]
+
+
+
+
+def thrice(func):
+    def inner(x,y):
+        n=3
+        while n>0:
+            print(func(x,y))
+            n-=1
+        #return func(x,y)
+    return inner
+
+@thrice
+def add(x,y):
+    return x+y
+
+add(5,6)
+'''
+
+
+class Animal():
+    def speak(self):
+        print(f'Animal is speaking')
+
+class Mammal():
+    def speak(self):
+        print(f'Mammal is speaking')
+
+class Dog(Animal,Mammal):
+    pass 
+
+obj = Dog()
+print(obj.speak())
