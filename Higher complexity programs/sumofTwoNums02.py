9d09212e79a476638c48de1f678a318f74fbed21
Harshad06 <harshad.shringi@gmail.com>
2021-07-08 17:24:48 +0530

    commit code

--- Higher complexity programs/sumofTwoNums02.py
+++ Higher complexity programs/sumofTwoNums02.py
@@ -1,24 +1,3 @@
-
-# sum 10 for adjacent 2 numbers??
-'''
-x = [3, 2, 4, 6, 5, 9, 1, 12, 10]
-
-for i in range(len(x)-1):
-    if x[i] + x[i+1] == 10:
-        print(f'Numbers are:', x[i], '&', x[i+1])
-    
-output:-        
-# Numbers are: 4 & 6
-# Numbers are: 9 & 1
--------------------------------
-error if :-
-if x[i] + x[i+1] == 10:
-IndexError: list index out of range
-it will take last element + 1, so it will OutOfIndex Error.
-'''
-
-
-
 # sum 10 for any 2 numbers??
 
 def indices_sum(nums,target):
