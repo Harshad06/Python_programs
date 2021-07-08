5bda8a9a511d2a1832b5128b6e84b1e7c133632a
Harshad06 <harshad.shringi@gmail.com>
2021-07-08 10:25:36 +0530

    commit

---
+++ TEST FILES/fujitsu.py
@@ -0,0 +1,37 @@
+
+# 2, 4 , 4, 12 
+# replace value of None by its current front number
+lst  =  [2,None,3,4,None,None,10,12,None]
+
+
+def seq(lst):
+    prev_char = None
+    
+    for current in seq:
+        if current == None:
+             current = prev_char
+        else:
+            pass
+        
+    return [lst]
+
+
+'''
+move all zeroes to the end:-
+
+lst  =  [1,7,0,0,10,12,0,5,6]
+
+l1 = [x for x in lst if x!=0]
+l2 = [x for x in lst if x==0]
+
+print(l1+l2)       # [1, 7, 10, 12, 5, 6, 0, 0, 0]
+'''
+
+'''
+from typing import Counter
+
+lst  =  ['one', 'two','three','one', 'two','three', 'one', 'four']
+
+d = Counter(lst)
+print(d)
+'''
