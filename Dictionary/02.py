e62340b86ac3d969583662a038d25730229f13fe
Harshad06 <harshad.shringi@gmail.com>
2021-07-26 12:51:22 +0530

    commit

--- Dictionary/02.py
+++ Dictionary/02.py
@@ -7,3 +7,24 @@ b.clear()       # -------> dict b contents cleared
 
 print(a)
 print(b)
+
+
+# ------------------------------------------------
+
+people = {
+          1: {'Name': 'John', 'Age': '27', 'Sex': 'Male'},
+          2: {'Name': 'Marie', 'Age': '22', 'Sex': 'Female'}
+        }
+
+#print(people.keys())
+#print(people.values())
+
+people[1]['Name'] = "JOHNNY"
+
+# print(people[1]['Name'])
+
+
+people[1]['NAAM'] = people[1]['Name']
+del people[1]['Name']
+
+print(people)
